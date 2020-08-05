21e5938d6baac2608a2e8c8573f5be2565b2069d
Agoston PAJZS <agostonp@gmail.com>
2020-08-05 11:32:49 +0200

    Convert Price class into a JPA Entity and generate all methods with Lombok

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -2,45 +2,27 @@ package com.udacity.pricing.domain.price;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class Price {
-
     private String currency;
     private BigDecimal price;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long vehicleId;
-
-    public Price() {
-    }
-
-    public Price(String currency, BigDecimal price, Long vehicleId) {
-        this.currency = currency;
-        this.price = price;
-        this.vehicleId = vehicleId;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Long getVehicleId() {
-        return vehicleId;
-    }
-
-    public void setVehicleId(Long vehicleId) {
-        this.vehicleId = vehicleId;
-    }
 }
