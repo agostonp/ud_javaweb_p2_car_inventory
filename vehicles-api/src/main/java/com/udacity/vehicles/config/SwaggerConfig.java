b7e9c597a0533509e710ec948ea432a57353a3c8
Agoston PAJZS <agostonp@gmail.com>
2020-08-06 16:08:54 +0200

    Add Swagger documentation

---
+++ vehicles-api/src/main/java/com/udacity/vehicles/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.udacity.vehicles.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Vehicle Inventory REST API",
+                "This API returns info from and allows modifications of a vehicle inventory database.",
+                "1.0",
+                "http://www.udacity.com/tos",
+                new Contact("Udacious Student", "www.udacity.com", "myeaddress@udacity.com"),
+                "License of API", "http://www.udacity.com/license", Collections.emptyList());
+    }
+}
