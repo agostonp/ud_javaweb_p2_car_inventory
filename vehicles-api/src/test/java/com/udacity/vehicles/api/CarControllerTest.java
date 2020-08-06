f8b3e6794b58bb3f57881ceb8ed30f6409188860
Agoston PAJZS <agostonp@gmail.com>
2020-08-06 22:42:34 +0200

    Add a test to check car update

--- vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -7,6 +7,7 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 //import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -133,6 +134,22 @@ public class CarControllerTest {
          */
         mvc.perform(delete("/cars/1"))
                 .andExpect(status().is2xxSuccessful()); 
+    }
+
+    /**
+     * Tests the update of a single car.
+     * @throws Exception if the update operation of a vehicle fails
+     */
+    @Test
+    public void updateCar() throws Exception {
+        Car car = getCar();
+        mvc.perform(
+                put(new URI("/cars/1"))
+                        .content(json.write(car).getJson())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
     /**
